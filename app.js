b493c505eca1f7a2dec35c77f0d5e5f0b3b079f5
SpyrosSiannas <ssiannas@gmail.com>
2021-04-30 19:45:13 +0300

    Minor Changes

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 
-app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 80,(error)=>{
-    if (error){
-        console.log('Something went wrong:',error);
-    } else {
-        console.log("Listening . . .")
-    }
-});
 app.engine('hbs', exphbs({
     layoutsDir: __dirname + '/views/layouts',
     extname: 'hbs',
     defaultLayout: 'main',
     partialsDir: __dirname + '/views/partials/'
 }));
+
+app.use(express.static(__dirname + '/public'));
+
+/** Heroku has the environment variable set 
+    but the way we locally run the server, we don't
+    so we set our local port to 80 and the heroku one
+    to the environment it's running
+*/
+app.listen(process.env.PORT || 80, (error)=>{
+    if (error){
+        console.log('Something went wrong:', error);
+    } else {
+        console.log("Listening . . .")
+    }
+});
 
 app.set('view engine', 'hbs');
 
