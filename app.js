4a197714a3452b4ace131f823fab8e595e019d96
SpyrosSiannas <ssiannas@gmail.com>
2021-04-30 14:41:54 +0300

    Added public assets folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/index.html', (req, res, next)=>{
     res.sendFile(__dirname + '/index.html')
