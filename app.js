fcfda0344d50eb472cb4ec5621640d6551a64f1f
SpyrosSiannas <ssiannas@gmail.com>
2021-04-30 19:28:00 +0300

    Added modularity and converted everything to templates

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res, next)=>{
-    res.sendFile(__dirname + '/index.html')
-})
 app.listen(process.env.PORT || 80,(error)=>{
     if (error){
         console.log('Something went wrong:',error);
     } else {
         console.log("Listening . . .")
     }
+});
+app.engine('hbs', exphbs({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        layout: 'main',
+        title:"Coffestained Records",
+        stylesheet: "index"
+    });
 });
