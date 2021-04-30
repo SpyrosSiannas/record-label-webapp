70165f0931f40ac2402f549c7e3e787a84e74060
SpyrosSiannas <ssiannas@gmail.com>
2021-04-30 14:39:08 +0300

    Modified app to use express

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-const http = require("http");
-const fs = require('fs')
-const port = 80;
+const express = require('express');
+const app = express();
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('index.html', (error, data) => {
-        if (error){
-            res.writeHead(404);
-            res.write("File not found!");
-        } else {
-            res.write(data);
-        }
-        res.end();
-    })
+app.use(express.static(__dirname));
+
+app.get('/index.html', (req, res, next)=>{
+    res.sendFile(__dirname + '/index.html')
 })
-
-server.listen(process.env.PORT,(error)=>{
+app.listen(process.env.PORT,(error)=>{
     if (error){
         console.log('Something went wrong:',error);
-    } else {
-        console.log('Server is listening on port ' + port)
     }
 });
