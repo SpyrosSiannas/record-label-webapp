3019e258e459968e35dd34ba4fc652e9d948aa96
SpyrosSiannas <ssiannas@gmail.com>
2021-04-30 14:48:27 +0300

    Modified so that server runs on port 80 locally

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/index.html', (req, res, next)=>{
+app.get('/', (req, res, next)=>{
     res.sendFile(__dirname + '/index.html')
 })
-app.listen(process.env.PORT,(error)=>{
+app.listen(process.env.PORT || 80,(error)=>{
     if (error){
         console.log('Something went wrong:',error);
+    } else {
+        console.log("Listening . . .")
     }
 });
