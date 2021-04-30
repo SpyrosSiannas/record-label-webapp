94869c3d4d129bdbec1e74ecc61404e28e15c29a
SpyrosSiannas <ssiannas@gmail.com>
2021-04-30 14:01:51 +0300

    Made changes and added gitignore

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
-const http = require("http")
-const port = 8080
+const http = require("http");
+const fs = require('fs')
+const port = 8080;
 
 const server = http.createServer(function(req, res) {
-
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', (error, data) => {
+        if (error){
+            res.writeHead(404)
+            res.write("File not found!")
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
 })
 
-server.listen(port, (error)=>{
+server.listen(port,(error)=>{
     if (error){
         console.log('Something went wrong:',error);
     } else {
